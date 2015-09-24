d8b70c3c6b3a89ab777ea71fadeaa1eebfeb265e
Nicolas Baya <nicolasbaya@gmail.com>
2015-09-24 14:33:34 +0200

    ex014

---
+++ exercises/014/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 18:14:41 2015
+
+@author: nico
+"""
+import sys
+if len(sys.argv) < 2:
+    print("usage: python3 solution.py PARAM")
+else:
+    print(sys.argv[1])
